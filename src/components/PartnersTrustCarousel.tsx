f9e94c5db0da2136b4168c315bf10a4bb4aca80f
souleymane1234 <konatesouleymane42@gmail.com>
2026-08-18 15:13:49 +0000

    update

--- src/components/PartnersTrustCarousel.tsx
+++ src/components/PartnersTrustCarousel.tsx
@@ -1,6 +1,42 @@
 import './PartnersTrustCarousel.css'
 
-const TRUST_LOGOS = [
+type TrustLogo = {
+  id: string
+  src: string
+  alt: string
+  href?: string
+}
+
+const PARTNER_HREFS: Record<string, string> = {
+  'AfrilandFirstBank.jpeg': 'https://afrilandfirstbankci.com/',
+  'Amoirie.png': 'https://www.education.gouv.ci/index.php/Welcome',
+  'BoisAfrique.jpeg': 'https://boisdafrique.com/',
+  'Booz.png': 'https://booztech.com/',
+  'CentreIvoirienRobotique.png': 'https://cirobotique.ci/',
+  'Daikin.png':
+    'https://www.daikinmea.com/fr_fr/dmea-sales-network/dmea-cote-divoire.html',
+  'Dolait.jpeg': 'https://sotigroup.com/produit/dolait-yaourt/',
+  'Eliwood.png': 'https://demo.eliwoodplay.com/',
+  'festo.webp': 'https://www.festo.com/za/en',
+  'Gandour.jpeg': 'https://web.facebook.com/Gandourci/?_rdc=1&_rdr#',
+  'Gipame.png': 'https://www.linkedin.com/in/gipame-abidjan/',
+  'GroupeCarreOr.jpg': 'https://www.carredor.org/',
+  'InnovTechnology.png': 'https://www.innovtechnology-ci.com/',
+  'Interform.png':
+    'https://annuaireci.com/en/entreprises/interform-ci-abidjan-cote-divoire/',
+  'OIPI.png': 'https://oipr-ci.org/',
+  'OmniTravaux.png': 'https://snedai.com/mot-cle/omni-travaux/',
+  'ortt.png': 'https://www.ongreveletontalent.com/',
+  'Prosuma.png': 'https://groupeprosuma.com/',
+  'Sogelus.png': 'https://www.sogelux.net/',
+  'Sotiv.png': 'https://ci.linkedin.com/in/sotiv-c%C3%B4te-d-ivoire-894786257',
+  'UniverselleIndustries.jpeg': 'https://universelleindustries.com/',
+  'Uniwax.png':
+    'https://www.uniwax.com/?srsltid=AfmBOorc2dnO1WMJQB4LQjcugEywisBQn93OpNjUtpfgQqqy6wlqNeU',
+  'WorldskillsIvoire.png': 'https://worldskills.org/',
+}
+
+const TRUST_LOGOS: TrustLogo[] = [
   'Interform.png',
   'Eliwood.png',
   'Dolait.jpeg',
@@ -24,12 +60,14 @@ const TRUST_LOGOS = [
   'WorldskillsIvoire.png',
   'Sotiv.png',
   'GroupeCarreOr.jpg',
+  'port-10.png',
 ].map((file) => {
   const name = file.replace(/\.[^.]+$/, '')
   return {
     id: name,
     src: `/trustCaroussel/${file}`,
     alt: name.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/[-_]/g, ' '),
+    href: PARTNER_HREFS[file],
   }
 })
 
@@ -56,23 +94,43 @@ export function PartnersTrustCarousel({ includeAnchorId = true }: PartnersTrustC
 
         <div className="partners-trust__viewport" aria-label="Logos partenaires">
           <div className="partners-trust__track">
-            {loopLogos.map((logo, index) => (
-              <div
-                key={`${logo.id}-${index}`}
-                className="partners-trust__item"
-                aria-hidden={index >= TRUST_LOGOS.length}
-              >
+            {loopLogos.map((logo, index) => {
+              const isDuplicate = index >= TRUST_LOGOS.length
+              const img = (
                 <img
                   className="partners-trust__logo"
                   src={logo.src}
-                  alt={index < TRUST_LOGOS.length ? logo.alt : ''}
+                  alt={isDuplicate ? '' : logo.alt}
                   width={180}
                   height={90}
                   loading="lazy"
                   decoding="async"
                 />
-              </div>
-            ))}
+              )
+
+              return (
+                <div
+                  key={`${logo.id}-${index}`}
+                  className="partners-trust__item"
+                  aria-hidden={isDuplicate}
+                >
+                  {logo.href ? (
+                    <a
+                      className="partners-trust__link"
+                      href={logo.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      tabIndex={isDuplicate ? -1 : undefined}
+                      aria-label={isDuplicate ? undefined : `${logo.alt} (ouvre le site)`}
+                    >
+                      {img}
+                    </a>
+                  ) : (
+                    img
+                  )}
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
